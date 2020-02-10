1b59e869bf5781ceec3562136febeebef8163775
Jay <psatdoctor@naver.com>
2020-02-10 20:21:01 +0900

    Pre Loading Assets

--- App.js
+++ App.js
@@ -1,22 +1,51 @@
 
 import React, {Component} from 'react';
+import {AppLoading} from "expo";
+import * as Font from 'expo-font';
+import {IonIcons} from "@expo/vector-icons";
 import {Text, View} from "react-native";
 
-
 class App extends Component {
-  constructor(props, ...rest) {
-    super(props, ...rest);
-    this.state = {};
+
+
+  constructor() {
+    super();
+    this.state = {
+      loaded: false
+    };
   }
 
+  handleError = error => console.log(error);
+
+  handleLoaded = () => this.setState({loaded: true});
+
+  loadAssets = async() => {
+    await Font.loadAsync({
+      ...IonIcons.font
+    });
+  };
+
+
+
   render() {
-    return (
+    const { loaded } = this.state;
 
-      <View>
-        <Text>Hello, World</Text>
-      </View>
-
-    );
+    if (loaded) {
+      return (
+        <View>
+          <Text>Open up App.js~~~~</Text>
+        </View>
+      );
+    } else {
+      return (
+        <AppLoading 
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleLoaded}
+        />
+      );
+    }
+    
   }
 }
 
