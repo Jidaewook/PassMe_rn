29d3064ec18718c030a1449a17af100b2644dd5b
Jay <psatdoctor@naver.com>
2020-02-10 20:04:39 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+
+import React, {Component} from 'react';
+import {Text, View} from "react-native";
+
+
+class App extends Component {
+  constructor(props, ...rest) {
+    super(props, ...rest);
+    this.state = {};
+  }
+
+  render() {
+    return (
+
+      <View>
+        <Text>Hello, World</Text>
+      </View>
+
+    );
+  }
+}
+
+
+export default App;
